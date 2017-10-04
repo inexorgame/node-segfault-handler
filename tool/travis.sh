3e93463e9b4f2eb43c5c8790a75880410a53417a
Croydon <cr0ydon@outlook.com>
2017-10-04 04:59:37 +0200

    Temporary workarounds are permanent

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+# FIXME: Get rid of the following workarounds, by NOT running this as sudo..
+ln -s /usr/local/bin/node /usr/bin/node
+ln -s /usr/local/lib/node /usr/lib/node
+ln -s /usr/local/bin/npm /usr/bin/npm
+ln -s /usr/local/bin/node-waf /usr/bin/node-waf
+
+
 npm install -g node-gyp
 node-gyp configure
-cd build && make
+mkdir make && cd build && make
