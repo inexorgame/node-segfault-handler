aae78474f3cdff31d152a54d3fac394680103854
Croydon <cr0ydon@outlook.com>
2017-10-04 06:00:45 +0200

    Fix

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
-# FIXME: Get rid of the following workarounds, by NOT running this as sudo..
-ln -s /usr/local/bin/node /usr/bin/node
-ln -s /usr/local/lib/node /usr/lib/node
-ln -s /usr/local/bin/npm /usr/bin/npm
-ln -s /usr/local/bin/node-waf /usr/bin/node-waf
-
-
+cd ..
 npm install -g node-gyp
 node-gyp configure
-mkdir make && cd build && make
+mkdir build && cd build && make
